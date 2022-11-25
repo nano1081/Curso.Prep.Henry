92563e239fd35485a23512df1cd4f2dbef39374f
nano1081 <legartojuan@gmail.com>
2022-11-25 15:08:29 -0300

    cambios

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,13 +1,13 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "juan" ;
+const nuevaString = "juan"git  ;
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 5000 ;
 
 // Crea una variable booleana:
-const nuevoBool = TRUE;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
